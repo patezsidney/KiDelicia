f2f18cfb4d120bcb8bbf228784d2c2e001a786b2
Sidney Patez <instalers.sa@gmail.com>
2021-12-06 17:07:53 -0300

    feat: clear code

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,22 @@
-let time = 4000
-    currentImageIndex = 0,
-    images= document
-                .querySelectorAll(".imagens-slider img")
-    max = images.length;
+let time = 7000;
+let currentImageIndex = 0;
+let images = document.querySelectorAll(".imagens-slider img");
+let max = images.length;
 
-    function nextImage() {
+function nextImage() {
+  images[currentImageIndex].classList.remove("selecionada");
 
-        images[currentImageIndex]
-            .classList.remove("selecionada")
+  currentImageIndex++;
 
-        currentImageIndex++
+  if (currentImageIndex >= max) currentImageIndex = 0;
 
-        if(currentImageIndex >= max)
-            currentImageIndex = 0
-
-        images[currentImageIndex]
-            .classList.add("selecionada")
-    }
-
-function start() {
-    setInterval(() => {
-        nextImage()
-    }, time)
+  images[currentImageIndex].classList.add("selecionada");
 }
 
-window.addEventListener("load", start)
+function start() {
+  setInterval(() => {
+    nextImage();
+  }, time);
+}
+
+window.addEventListener("load", start);
